c6cb6efba259f4b2284455fd0ad8603c128a9702
I Siddhartha Kumar <iskumar789@gmail.com>
2017-12-04 19:47:56 +0530

    spi port init code

---
+++ spi_interface_C/spi.c
@@ -0,0 +1,11 @@
+#include <bcm2835.h>
+void main(){
+	if (!bcm2835_init())
+    {
+      printf("bcm2835_init failed. Are you running as root??\n");
+    }
+    if (!bcm2835_spi_begin())
+    {
+      printf("bcm2835_spi_begin failed. Are you running as root??\n");
+    }
+}
